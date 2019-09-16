6dcef1a8457aad3eb946b156fe634f3c0cab9f8a
Jordi <jordicasesnoves@gmail.com>
2019-09-16 14:36:03 +0200

    changed doc title

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,21 @@ import Home from '@/components/Pages/Home';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: Home,
     }
   ]
 });
+
+/* Use this when you need to change doc title for each route
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})*/
+
+export default router
